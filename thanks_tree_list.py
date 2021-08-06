5a6ae49c9fd5e8bb898cdf4066bdca0c5f51887f
bilded <cjsfhdurwjd3382@naver.com>
2021-08-06 13:47:20 +0900

    나무 만들기 초반

---
+++ thanks_tree_list.py
@@ -0,0 +1,33 @@
+
+{
+  "version": "2.0",
+  "template": {
+    "outputs": [
+      {
+        "basicCard": {
+          "title": "레벨 n단계",    #4번 반복
+          "description": "이 안에 어떤 식물이 있을까?",
+          "thumbnail": {    #썸네일
+            "imageUrl": "http://upload2.inven.co.kr/upload/2017/03/23/bbs/i13409223033.jpg"
+          },
+          "profile": {
+            "imageUrl": "https://postfiles.pstatic.net/MjAyMTA4MDVfMjc0/MDAxNjI4MTU3NDU4NzMx.GcxhVT_QU1ObJUYxFtBKOX6053rETsAYw5lFjPnWAHAg.wNcJlXhPPLJLC4G1u6IDMwsFXhTD-k4iFywOSWc6Ngwg.JPEG.lovecat3631/output_2450324132.jpg?type=w773",
+            "nickname": "꽃"
+          },
+          "social": {       #이게 무엇?
+            "like": 1238,
+            "comment": 8,
+            "share": 780
+          },
+          "buttons": [
+            {
+              "action": "message",
+              "label": "식물보기",
+              "messageText": "짜잔 아직은 N까지 성장했네요  조금 더 키워봐요!"
+            },
+          ]
+        }
+      }
+    ]
+  }
+}
