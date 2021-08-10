cac121f77184dfc77b089a85e77feba417a2f003
bilded <cjsfhdurwjd3382@naver.com>
2021-08-10 13:49:57 +0900

    데이터베이스 파일 생성

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
+from flask import current_app as app
+from models import db, migrate
 from flask import Flask, request
 import word_by_situation
 import situation_word
 import Today_Word
 import situation_word
+import os
 
 
 app = Flask(__name__)       #웹서버 이름
+
+BASE_DIR = os.path.dirname(__file__)
+dbfile = os.path.join(BASE_DIR, 'db.sqlite')
+
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///'+dbfile
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']= True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+app.config['SECRET_KEY']='alskejfmv'
+
+db.init_app(app)
+migrate.init_app(app, db, render_as_batch=True)
+db.app =app
+db.create_all()
+
+
 
 @app.route('/')             #페이지 주소
 def hello():                #페이지에서 띄울 내용
--- models.py
+++ models.py
@@ -1,21 +1,44 @@
-from flask.scaffold import F
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from flask_migrate import Migrate
 
-db = SQLAlchemy()
+naming_convention = {
+    "ix": "ix_%(column_0_lable)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+db = SQLAlchemy(metadata=MetaData(naming_convention = naming_convention))
+migrate = Migrate()
+
 
 class user(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     userid = db.Column(db.String(32), nullable=False)
+    partners = db.relationship('thanks_list', backref = 'user')  #
 
-    #password = db.Column(db.string(128), nullable=False)
+    def __repr__(self):
+        return '<user %r>' % self.id
 
-    def __init__(self, userid, password):
+    def __init__(self, userid):
         self.userid = userid
-        self.password = password
 
 class thanks_list(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     thanks = db.Column(db.String(128), nullable = False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))  # db.ForeignKey('user.id') 가져온다.
-    user = db.relationship("user", backref = "user.id")
+    
+    def __repr__(self):
+        return '<thanks_list %r>' % self.id
+
+    def __init__(self, user_id, thanks):
+        self.user_id = user_id
+        self.thanks = thanks
+
+
+
+    
+    
